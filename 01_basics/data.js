37f84976164f1b92cea2beffcccd3aa98bd0c9fa
Md. Shah Masud <mdshahmasud@gmail.com>
2024-10-07 09:03:59 +0000

    datatype

--- 01_basics/data.js
+++ 01_basics/data.js
@@ -1,7 +1,11 @@
 
-let number = 45;
-let stringNumber = String(number);
+// let number = 45;
+// let stringNumber = String(number);
 
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
 
+let isLoggedIn= "masud";
+console.log (typeof(isLoggedIn));
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
